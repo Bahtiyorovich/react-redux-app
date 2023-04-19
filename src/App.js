06855c5c998ab6af2220ef6f509cb0405c03ba20
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-04-19 22:31:32 +0500

    news

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Login, Main, Navbar, Register } from "./components"
 import { getItem } from "./helpers/persistance-storage"
 import { signUserSuccess } from "./slice/auth"
 import AuthService from "./service/auth"
+import ArticleService from './service/article';
+import { getArticleStart, getArticleSuccess } from "./slice/article"
 
 const App = () => {
 
@@ -19,11 +21,23 @@ const App = () => {
     }
   }
 
+  const getArticles = async () => {
+    dispatch(getArticleStart())
+    try{
+      const response = await ArticleService.getArticles()
+      dispatch(getArticleSuccess(response.articles))
+    } catch(error){
+      console.log(error)
+    }
+  }
+
   useEffect(() => { 
     const token = getItem('token')
     if(token){
       getUser()
     }
+
+    getArticles()
   },[])
 
   return (
